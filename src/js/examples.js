102ddc825471f172e41b0ae2215c4468dcd537fa
Yukino Song <syqlds@126.com>
2017-04-22 02:13:18 +0800

    Add simple example page

--- src/js/examples.js
+++ src/js/examples.js
@@ -6,12 +6,42 @@ import classes from '../styles/examples.css'
 // Import template
 import _examples from '../templates/examples.eft'
 
+const selected = `${classes.item} ${classes.selected}`
+
+const info = {
+	todo: {
+		src: 'https://classicoldsong.github.io/todomvc-efjs/',
+		repo: ''
+	},
+	dbmon: {
+		src: 'https://classicoldsong.github.io/js-repaint-perfs/ef/index.html',
+		repo: ''
+	},
+	site: {
+		src: location.origin,
+		repo: ''
+	}
+}
+
+const show = ({state, value}) => {
+	const newClasses = {
+		todo: classes.item,
+		dbmon: classes.item,
+		site: classes.item
+	}
+	newClasses[value] = selected
+	state.$data.class = newClasses
+	state.$data = info[value]
+}
+
 const examples = _examples.render({
 	$data: {
 		class: sharedClasses
-	}
+	},
+	$methods: { show }
 })
 
 examples.$data.class = classes
+show({state: examples, value: 'todo'})
 
 export default examples
