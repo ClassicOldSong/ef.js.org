f24b5a0e04010ff7b74b1d41499bac6ef7d3f64f
Yukino Song <syqlds@126.com>
2017-04-25 04:39:49 +0800

    Use page.js for routing

--- src/js/router.js
+++ src/js/router.js
@@ -1,4 +1,5 @@
 // Import utilities
+import page from 'page'
 import domReady from './utils/domready.js'
 // Import style
 import headerClass from '../styles/header.css'
@@ -12,6 +13,8 @@ import guide from './guide.js'
 import api from './api.js'
 import examples from './examples.js'
 
+// page('/#!')
+
 window.mainbody = mainbody
 
 console.log(mainbody)
@@ -19,64 +22,67 @@ console.log(mainbody)
 const classSelected = `${headerClass.link} ${headerClass.selected}`
 
 const goto = ({value}) => {
-	if (!(/^#!\//).test(value)) value = `#!/${value}`
-	window.location.hash = value
+	page(value)
 	window.scrollTo(0, 0)
 }
 
 const open = ({value}) => window.open(value)
 
 const changePage = (path) => {
-	const newClasses = {
-		examples: headerClass.link,
-		api: headerClass.link,
-		guide: headerClass.link,
-		home: headerClass.link,
-	}
+	if (path) header.$data.class[path] = classSelected
 
-	switch (path) {
-		case '#!/examples': {
-			newClasses.examples = classSelected
-			mainbody.body = examples
-			break
-		}
-		case '#!/api': {
-			newClasses.api = classSelected
-			mainbody.body = api
-			break
-		}
-		case '#!/guide': {
-			newClasses.guide = classSelected
-			mainbody.body = guide
-			break
-		}
-		case '#!/home': {
-			newClasses.home = classSelected
-			mainbody.body = home
-			break
-		}
-		default: {
-			notfound.$refs.box.classList.remove(notfoundClass.hidden)
-			mainbody.body = notfound
-			window.requestAnimationFrame(() => notfound.$refs.box.classList.add(notfoundClass.hidden))
-		}
-	}
-
-	header.$data.class = newClasses
 	header.$refs.overlay.classList.remove(headerClass.hidden)
 	window.requestAnimationFrame(() => header.$refs.overlay.classList.add(headerClass.hidden))
 }
+
+page('/*', (ctx) => {
+	if (ctx.path === '/') page.redirect('home')
+	else page.redirect(ctx.path.slice(1))
+})
+page('home', () => {
+	mainbody.body = home
+	changePage('home')
+})
+page('guide', () => {
+	mainbody.body = guide
+	changePage('guide')
+})
+page('api', () => {
+	mainbody.body = api
+	changePage('api')
+})
+page('examples', () => {
+	mainbody.body = examples
+	changePage('examples')
+})
+page('guide/:ref', (ctx) => {
+	mainbody.body = guide
+	changePage('guide')
+	window.requestAnimationFrame(() => {
+		guide.$data.ref = ctx.params.ref
+	})
+})
+page('*', () => {
+	notfound.$refs.box.classList.remove(notfoundClass.hidden)
+	mainbody.body = notfound
+	window.requestAnimationFrame(() => notfound.$refs.box.classList.add(notfoundClass.hidden))
+	changePage()
+})
+page.exit('*', (ctx, next) => {
+	header.$data.class[ctx.path.split('/')[0]] = headerClass.link
+	next()
+})
+
+page({hashbang: true})
 
 header.$methods.goto = goto
 examples.$methods.open = open
 home.$methods = { goto, open }
 
-window.addEventListener('hashchange', () => changePage(location.hash))
+// window.addEventListener('hashchange', () => changePage(location.hash))
 
 const init = () => {
 	document.querySelector('body').appendChild(mainbody.$element)
-	if (location.hash) changePage(location.hash)
-	else goto({value: 'home'})
 	header.$refs.overlay.classList.remove(headerClass.hidden)
 }
 
@@ -89,4 +95,4 @@ window.addEventListener('load', onload)
 
 domReady(init)
 
-export { goto, open, changePage }
+export { goto, open }
