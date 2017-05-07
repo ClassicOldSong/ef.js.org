a39f50d6c3f37f03223b9113274f01d51d0f97bc
Yukino Song <syqlds@126.com>
2017-05-07 20:34:32 +0800

    Fix path for guides.js

--- src/js/guides.js
+++ src/js/guides.js
@@ -1,12 +1,14 @@
 import classes from '../styles/guides.css'
-import _quickStart from './guides/quick-start.js'
+import _quickStart from '../templates/guides/quick-start.js'
+import _EFML from '../templates/guides/efml.js'
 
 const applyStyle = (state) => {
 	state.$data.class = classes
 }
 
 const guides = {
-	_quickStart
+	_quickStart,
+	_EFML
 }
 
 export {applyStyle, guides}
