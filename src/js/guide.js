af3a1f9de6810debbb03c5b0ab10000f7203923c
Yukino Song <syqlds@126.com>
2017-06-17 00:55:12 +0800

    Update

--- src/js/guide.js
+++ src/js/guide.js
@@ -37,97 +37,43 @@ const items = [
 				code4: Prism.highlight(code4, efmlSyntax)
 			}
 		}
-	},
-	{
+	}, {
 		title: 'EFML',
 		ref: 'efml',
 		content: _guides._EFML
-	},
-	{
+	}, {
 		title: 'Basic Binding',
 		ref: 'basic-binding',
 		content: _guides._basicBinding
-	},
-	{
+	}, {
 		title: 'Two-way Binding',
 		ref: 'two-way-binding',
 		content: _guides._twoWayBinding
-	},
-	{
+	}, {
 		title: 'Events handling',
 		ref: 'events-handling',
 		content: _guides._eventsHandling
-	},
-	{
+	}, {
 		title: 'Mounting',
 		ref: 'mounting',
 		content: _guides._mounting
-	},
-	{
+	}, {
 		title: 'Bundled Rendering',
 		ref: 'bundled-rendering',
 		content: _guides._bundledRendering
+	}, {
+		title: 'Modulelize',
+		ref: 'modulelize',
+		content: ''
+	}, {
+		title: 'Dig into ef',
+		ref: 'dig-into-ef',
+		content: ''
+	}, {
+		title: 'AST Structure',
+		ref: 'ast-structure',
+		content: ''
 	}
-	// {
-	// 	title: '» Tags',
-	// 	ref: 'tags',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Text Binding',
-	// 	ref: 'text-binding',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Attribute Binding',
-	// 	ref: 'attribute-binding',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Property Binding',
-	// 	ref: 'property-binding',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Event Binding',
-	// 	ref: 'event-binding',
-	// 	content: ''
-	// },
-	// {
-	// 	title: 'Advanced Usage',
-	// 	ref: 'advanced-usage',
-	// 	content: 'Advanced usage here'
-	// },
-	// {
-	// 	title: '» Classname Handling',
-	// 	ref: 'classname-handling',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Style Handling',
-	// 	ref: 'style-handling',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Two way Binding',
-	// 	ref: 'two-way-binding',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» Modulelize',
-	// 	ref: 'modulelize',
-	// 	content: ''
-	// },
-	// {
-	// 	title: 'Dig into ef',
-	// 	ref: 'dig-into-ef',
-	// 	content: ''
-	// },
-	// {
-	// 	title: '» AST Structure',
-	// 	ref: 'ast-structure',
-	// 	content: ''
-	// }
 ]
 
 for (let i of items) {
